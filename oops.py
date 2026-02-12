8b4cedb5983f9144a7b1b9663dc4f9ebbe6a7142
kelvin <kelvinn0305@gmail.com>
2026-02-12 09:45:42 +0530

    project4

--- oops.py
+++ oops.py
@@ -106,17 +106,227 @@
 # obj1.display()
        
     
-class bank:
+
+# Inheritance
+
+#  singale inheritance  
+
+# class animal:
+#     def sound(self):
+#         print("animal")
+        
+# class dog(animal):
+#     def speaking(self):
+#         print("break")
+        
+# obj = dog()
+
+
+# obj.sound()
+# obj.speaking()
+
+
+#  multiple inheritance
+
+# class animal:
     
-    def __init__(self,name,accNum,balance):
-        self.name=name
-        self.accNum=accNum
-        self.balance=balance
+#     def sound(self):
+#         print("animal")
         
-        print("accoumt created !")
+# class dog:
+#     def speaking(self):
+#         print("break")
         
-    def getinfo(self):
-        print("name :",{self.name},"account number )
+# class cat(animal,dog):
+#     def speaking1(self):
+#         print("meow")
         
+# obj = cat()
+
+# obj.sound() 
+# obj.speaking()
+# obj.speaking1()
+
+# multi level inheritance
+
+# class animal:
+#     def sound(self):
+#         print("animal")
         
+# class dog(animal):
+#         def speaking(self):
+#             print("beark")
+            
+# class cat(dog):
+#          def speaking1(self):
+#              print("meow")
+             
+# obj = cat()
+
+# obj.sound()
+# obj.speaking()
+# obj.speaking1()
+
+
+# hierarchical inheritance
+      
+# class  animal:
+#     def sound(self):
+#         print("animal")
+        
+# class dog(animal):
+#     def speaking(self):
+#         print("break")
+        
+# class cat(animal):
+#     def speaking1(self):
+#         print("meow")
+        
+# obj = cat()
+# obj1 = dog()
+
+# # obj.sound() 
+# obj1.speaking()
+# obj.speaking1()
+
+# polymorphism 
+
+# method overridng
+# method overloading
+# operator overloading
+
+# method overriding
+
+# class animal:
+#     def sound(self):
+#         print("animal")
+        
+# class dog(animal):
+#     def sound(self):
+#         print("dog")
+
+# class cat(animal):
+#     def sound(self):
+#         print("cat") 
+        
+# obj = cat()
+
+# obj.sound()
+
+
+# overlodaing
+
+# class math:
+#     def __init__(self,marks=30):
+#         self.marks=marks
     
+#     def __add__(self,other):                                                                                                                           
+#         return self.marks + other.marks
+    
+#     def __str__(self):
+#         return str(self.marks)
+# obj = math(20)
+# obj1 = math(30) 
+# obj2 = math(20)
+
+
+
+class car:
+    def __init__(self,brandname,modelname):
+        self.brandname=brandname
+        self.modelname=modelname
+        
+    def showcarinfo(self):
+            print(f"the car brand is{self.brandname} and the model is {self.modelname}")
+            
+class mahindra(car):
+    def __init__(self,brandname,modelname,turbo):
+        super().__init__(brandname,modelname)
+        self.turbo=turbo
+        
+    def showcarinfo(self):
+        print(f"the car brand is {self.brandname} and the model is {self.modelname} and the turbo is {self.turbo}")
+        
+class toyota(car):
+    def __init__(self,brandname,modelname,seats):
+        super().__init__(brandname,modelname)
+        self.seats=seats
+        
+    def showcarinfo(self):
+        print(f"the car brand is {self.brandname} and the model is {self.modelname} and the seats no. is{self.seats}")
+        
+        
+Bcars = []
+Tcars = []
+Mcars = []
+
+while True:
+    
+    print("\n Enter 1 the add basic car ")
+    print(" Enter 2 the add mahindar car ")
+    print(" Enter 3 the add toyota car")
+    print(" Enter 4 the show all cars")
+    print(" Enter 5 to exit")
+        
+    choice = int(input("Enter your choice: "))
+    
+    if choice == 1:
+        brandname = input("Enter the brand name: ")
+        modelname = input("Enter the mdoel name: ")
+         
+        car1 = car(brandname,modelname)
+        Bcars.append(car1)
+        
+    elif choice == 2:
+        barndname = input("Enter the barnd name: ")
+        modelname = input("Enter the model name:")
+        turbo = input("turbo is avaiable yes/no :")
+        
+        car2 = mahindra(brandname,modelname,turbo)
+        Mcars.append(car2)
+        
+    elif choice == 3:
+        brandname = input("Enter the brandname : ")
+        modelname = input("Enter the modelname : ")
+        seats  = input("Enter the seats no. : ")
+        
+        cars3 = toyota(brandname,modelname,seats)
+        Tcars.append(cars3)
+        
+    elif choice == 4:
+        print(" Enter 1 the add basic car ")
+        print(" Enter 2 the add mahindar car ")
+        print(" Enter 3 the add toyota car")
+        
+        Achoice = int(input("Enter your choice"))
+        
+        if Achoice == 1:
+             for car1 in Bcars:
+                 car1.showcarinfo()
+            
+            
+        elif Achoice == 2:     
+             for car2 in Mcars:
+                car2.showcarinfo()
+                
+        elif Achoice == 3:       
+             for car3 in Tcars:
+                 car3.showcarinfo()
+                 
+        else: 
+            print("\n anvalid choice")
+            
+    elif choice == 0:
+        print("\n exiting ")
+        break
+        
+    else:
+        print("\n invalid choice \n")
+
+            
+        
+        
+        
+        
+        
+        
---
+++ pro5.py
@@ -0,0 +1,20 @@
+print(" --- python OOP project: employee Management System --- ")
+
+class Employee:
+    __Employee_id=None
+    __name=None
+    __age=None
+    __salary=None
+    
+    def setter(self,Employee_id,name,age,salary):
+        self._Employee_id=Employee_id
+        self._name=Name
+        self._age=age
+        self._salary=salary
+        
+        
+    def getter(self):
+        print(self._Employee_id,self._name,self._age,self._salary)
+        
+        
+    
