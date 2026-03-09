6b9022ff1ef4e39b032c010e4fb0c13ac841b154
kelvin <kelvinn0305@gmail.com>
2026-03-09 12:30:41 +0530

    numpy1

--- README.md
+++
@@ -1,4 +0,0 @@
-"# python" 
-"# python" 
-"# python1" 
-"# python1" 
---
+++ project8.py
@@ -0,0 +1,27 @@
+print("welcome to the numpy analyzer")
+print("="*40)
+
+class DataAn:
+    
+    def array(self):
+        num = input("enter a 1D array: ")
+        arr = list(map(int,num.split()))
+        a = np.array(arr)
+            
+            
+            
+            
+            
+            
+    
+while True:
+    
+    print("choose an option:")
+    print("1. create a numpy array")
+    print("2. perform mathematical oprations")
+    print("3. combine pr split Array")
+    print("4. search,sort,or filter arrays")
+    print("5. compute aggegates and statistics")
+    print("6. exit")
+    
+    
